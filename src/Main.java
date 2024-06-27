16b419bada20d83440a3649accb32c62249af10b
jeongmin <jeongmin0046@gmail.com>
2024-06-27 18:14:58 +0900

    oio

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
 
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+        System.out.println("hello world3");
+        System.out.println("hello world4");
+        System.out.println("hello world5");
+        System.out.println("hello world6");
     }
 
 }
