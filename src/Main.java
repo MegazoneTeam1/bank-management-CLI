e2439cde8424f2269397d4bb0bf74730430b3fef
jy <ju79318@naver.com>
2024-06-27 18:43:27 +0900

    rebase

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("go-marr");
 
     }
 
 }
+
+지금
+git checkout go-marr
+git rebase main 까지 한 상태인거지?
+
+좌측에 Commit 누르면 conflict 관련 뭐 안 떠 ?
+commit 누르고 뜨는 거 사진 찍어서 보내주라
