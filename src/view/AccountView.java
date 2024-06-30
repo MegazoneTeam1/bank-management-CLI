b32b82444ac1746df9e8b050a2661041d51bafc9
jeongmin <jeongmin0046@gmail.com>
2024-06-30 21:40:59 +0900

    feat: AccountView 생성

---
+++ src/view/AccountView.java
@@ -0,0 +1,36 @@
+package view;
+
+import domain.Account;
+
+public class AccountView {
+    public void showAccount() {
+
+        String[] options = {"계좌 생성", "계좌 조회", "잔액 조회", "입금", "출금","로그아웃"};
+
+        while (true) {
+            PrintUtil.displayMenu(options);
+            int choice = PrintUtil.readInt("뭐할거임?");
+            switch(choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    return;
+                default:
+                    PrintUtil.println("잘못된 입력입니다.");
+                    PrintUtil.println("");
+            }
+
+        }
+
+    }
+
+}
+
