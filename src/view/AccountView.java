31b90329e09a40d679c48763650dbcf39c28b194
jy <ju79318@naver.com>
2024-07-01 17:16:23 +0900

    refactor:메서드 명명 규칙에 맞게 정정

--- src/view/AccountView.java
+++ src/view/AccountView.java
@@ -10,16 +10,16 @@ public class AccountView {
     public void printAccount(int accountNumber){
         PrintUtil.println("생성된 계좌번호는 "+accountNumber +" 입니다.");
     }
-    public String printPw(){
+    public String printPassword(){
         return PrintUtil.readLine("비밀번호 설정 (4자리) :");
     }
-    public String printPwAgain() {
+    public String printPasswordAgain() {
         return PrintUtil.readLine("4자리가 아닙니다.\n다시 입력해주세요. : ");
     }
-    public String printCheckPw(){
+    public String printCheckPassword(){
         return PrintUtil.readLine("비밀번호 재확인 : ");
     }
-    public void wrongPw() {
+    public void wrongPassword() {
         PrintUtil.print("설정한 비밀번호와 일치하지 않습니다.");
     }
 
