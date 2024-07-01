4a0c9adebd2f35ca14444d89f155afa096526fe1
jy <ju79318@naver.com>
2024-07-01 16:43:01 +0900

    refactor : 불필요한 인스턴스 삭제, 코드 리팩토링(상수 변경, 클래스내에서 객체 선언, 테스트 부분 삭제)

--- src/service/AccountServiceImpl.java
+++ src/service/AccountServiceImpl.java
@@ -4,94 +4,90 @@ import domain.Account.Account;
 import domain.User;
 import repository.AccountRepositoryImpl;
 import view.AccountView;
-import view.PrintUtil;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class AccountServiceImpl implements AccountService {
-    final private int ACCOUNTLENGTH = 1000000000;
+    final static private int ACCOUNTLENGTH = 1000000000;
+    final static private int PWLENGTH = 4;
     AccountView accountView = new AccountView();
-    PrintUtil printUtil = new PrintUtil();
+    AccountRepositoryImpl accountRepository = new AccountRepositoryImpl();
+
     @Override
     public boolean createAccount(Account account) {
         User user = new User();
 
         accountView.startPrint();
+        //계좌 10자리 랜덤 생성
         setAccountNum(account);
 
-
         //비밀번호 사용자에게서 받기
-        String password = accountView.printPw();
+        String password = getPwToUser();
 
-        //비밀번호 4자리 체크
-        while(checkPasswordLength(password)){
-            //다시 입력
-            password = accountView.printPwAgain();
-        }
         //비밀번호 삽입
         account.setPassword(password);
 
         //비밀번호 확인
-        while (checkPassword(password));
+        checkPasswordAgain(password);
 
-        //게좌생성, 개좌저장소에 저장소
-        AccountRepositoryImpl accountRepository = new AccountRepositoryImpl();
+        //계좌생성, 계좌저장소에 저장소
         accountRepository.save(account);
         return false;
     }
 
     @Override
+    //아직 미구현
     public List<Account> getAccountsByUserId(String userId) {
         List<Account> accounts = new ArrayList<>();
         return List.of( );
     }
-
+    //아직 미구현
     @Override
     public double getTotalBalance(String userId) {
         return 0;
     }
-
+    /**@계좌번호 중복 확인*/
     public boolean checkAccountNum(String accountNumber){
-        AccountRepositoryImpl accountRepository = new AccountRepositoryImpl();
-        Account a = accountRepository.findByAccountNumber(accountNumber);
-        if(a == null){ return false; }
-        else return true;
+        return accountRepository.findByAccountNumber(accountNumber)!= null;
+    }
 
+    /**@비밀번호 4자리 체크*/
+    public String checkPasswordLength(String password){
+        while(password.length() != PWLENGTH){
+            //다시 입력
+            password = accountView.printPwAgain();
+        }
+        return password;
     }
-    public boolean checkPasswordLength(String password){
-        if(password.length() !=4) return true;
-        return false;
+    /**@비밀번호를 사용자로부터 받기*/
+    public String getPwToUser(){
+        String password = accountView.printPw();
+        return checkPasswordLength(password);
     }
-    public boolean checkPassword(String password){
+    /**@비밀번호 재확인*/
+    public boolean checkPasswordAgain(String password) {
         String checkPw = accountView.printCheckPw();
-        if(checkPw.equals(password)) return false;
-        else {
+        while(!checkPw.equals(password)){
             accountView.wrongPw();
-            return true;
+            checkPw = accountView.printCheckPw();
         }
+        return true;
     }
-
+   /**@계좌 생성에 삽입*/
     public void setAccountNum(Account account){
-        //계좌 랜덤 번호 생성
-        int accountNumber = (int) (Math.random() * ACCOUNTLENGTH)+ACCOUNTLENGTH;
-        //계좌 중복 체크
-        while (checkAccountNum(String.valueOf(accountNumber))) {
-            accountNumber = (int) (Math.random() * ACCOUNTLENGTH)+ACCOUNTLENGTH;
-        }
-        accountView.printAccount(accountNumber);
+        //계좌 번호 생성
+        String accountNumber = generateUniqueAccountNumber();
+        accountView.printAccount(Integer.parseInt(accountNumber));
         //계좌 번호 삽입
-        account.setAccountNumber(String.valueOf(accountNumber));
-
-
+        account.setAccountNumber(accountNumber);
     }
-
-    public static void main(String[] args) {
-        Account account = new Account();
-        AccountServiceImpl accountService = new AccountServiceImpl();
-
-        accountService.createAccount(account);
-
+    /**@계좌번호 랜덤으로 10자리 받기*/
+    public String generateUniqueAccountNumber(){
+        int accountNumber;
+        do {
+            accountNumber = (int) (Math.random() * ACCOUNTLENGTH)+ACCOUNTLENGTH;
+        }while (checkAccountNum(String.valueOf(accountNumber)));
+        return String.valueOf(accountNumber);
     }
 
 }
