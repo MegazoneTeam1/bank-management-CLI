e0d31bf30216e183363b7c7783726e8b5cb47700
jy <ju79318@naver.com>
2024-07-01 16:44:00 +0900

    comment: 주석추가

--- src/repository/AccountRepositoryImpl.java
+++ src/repository/AccountRepositoryImpl.java
@@ -9,21 +9,23 @@ public class AccountRepositoryImpl implements AccountRepository {
     private static List<Account> accounts = new ArrayList<>();
 
     @Override
+    /**@생성된 계좌 저장*/
     public void save(Account account) {
         accounts.add(account);
     }
 
     @Override
+    /**@계좌번호 검색*/
     public Account findByAccountNumber(String accountNumber) {
-
         return accounts.stream().filter(account -> account.getAccountNumber().equals(accountNumber)).findFirst().orElse(null);
     }
 
     @Override
+    //미구현
     public List<Account> findAllByUserId(String userId) {
         return List.of();
     }
-
+    //미구현
     @Override
     public void update(Account account) {
 
