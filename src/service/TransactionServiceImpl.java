898cf1e3a560125b3bd608404bb5873b3e0e54a6
Dowon <tnstjs1919@gmail.com>
2024-07-02 21:06:14 +0900

    feat: 입금메서드,입금관련 view

--- src/service/TransactionServiceImpl.java
+++ src/service/TransactionServiceImpl.java
@@ -2,27 +2,45 @@ package service;
 
 import domain.Account;
 import domain.Transaction;
-import view.ViewTransactionService;
+import repository.AccountRepositoryImpl;
+import repository.TransactionRepositoryImpl;
+import view.DepositeView;
 
 import java.util.List;
 
 public class TransactionServiceImpl implements TransactionService {
 
+    private final DepositeView depositeView;
+    private final TransactionRepositoryImpl transactionRepository;
+    Transaction transaction = new Transaction();
     Account account = new Account();
-    public TransactionServiceImpl() {
-        super();
+
+    public TransactionServiceImpl(DepositeView depositeView, TransactionRepositoryImpl transactionRepository) {
+        this.depositeView = depositeView;
+        this.transactionRepository = transactionRepository;
     }
 
     @Override
     public boolean deposit(String accountNumber, double amount) {
+
+        // 입금 시작
+        depositeView.startDeposite();
+        // 입금 계좌 금액 입력
+        depositeView.depositeAccount(accountNumber);
+        depositeView.depositeAmount(amount);
+
         if(account.getAccountNumber().equals(accountNumber)){
             account.setBalance(account.getBalance() + amount);
         }
+        transactionRepository.save(transaction);
+        depositeView.depositsucess();
+
         return false;
     }
 
     @Override
     public boolean withdraw(String accountNumber, String password, double amount) {
+
         if(account.getAccountNumber().equals(accountNumber) && account.getPassword().equals(password)){
             if(account.getBalance() >= amount) {
                 account.setBalance(account.getBalance() - amount);
@@ -39,10 +57,5 @@ public class TransactionServiceImpl implements TransactionService {
         return null;
     }
 
-    // 입금 입력받으면 계좌번호, 입금금액 날짜 총금액 출력 메서드
 
-
-    // 출금 입력받으면 계좌번호, 출금금액 날짜 총금액 출력 메서드
-
-    //상대계좌? - 입금메서드
 }
---
+++ src/view/DepositeView.java
@@ -0,0 +1,49 @@
+package view;
+
+import domain.Account;
+import repository.AccountRepositoryImpl;
+import repository.TransactionRepository;
+
+import java.util.Scanner;
+
+public class DepositeView {
+
+    // 입금 시작
+    public void startDeposite (){
+        PrintUtil.println("입금을 시작합니다.");
+    }
+
+    // 입금 계좌 입력
+    public String depositeAccount (String accountNumber){
+       return PrintUtil.readLine("입금할 계좌번호를 입력하세요 :");
+    }
+
+    // 입금 금액 입력
+    public int depositeAmount(double amount){
+        return PrintUtil.readInt("입금할 금액을 입력하세요");
+    }
+
+    // 출금 계좌 입력
+    public String withdrawAccount (String accountNumber){
+        return PrintUtil.readLine("출금할 계좌번호를 입력하세요: ");
+    }
+    //출금 금액 입력
+    public int withdrawAmount(int amount){
+        return PrintUtil.readInt("출금할 금액을 입력하세요");
+    }
+    // 입금 성공
+    public void depositsucess(){
+         PrintUtil.println("입금이 성공적으로 되었습니다.");
+
+    }
+
+    }
+
+
+
+
+
+
+
+
+
