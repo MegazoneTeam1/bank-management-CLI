4001921a18b4407e224918dd559ec70fa0cbc166
jy <ju79318@naver.com>
2024-07-02 17:30:28 +0900

    feat:계좌 잔액 조회 기능 구현

--- src/repository/AccountRepositoryImpl.java
+++ src/repository/AccountRepositoryImpl.java
@@ -4,26 +4,27 @@ import domain.Account.Account;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AccountRepositoryImpl implements AccountRepository {
     private static List<Account> accounts = new ArrayList<>();
 
     @Override
-    /**@생성된 계좌 저장*/
+    /**생성된 계좌 저장*/
     public void save(Account account) {
         accounts.add(account);
     }
 
     @Override
-    /**@계좌번호 검색*/
+    /**계좌번호 검색*/
     public Account findByAccountNumber(String accountNumber) {
         return accounts.stream().filter(account -> account.getAccountNumber().equals(accountNumber)).findFirst().orElse(null);
     }
 
     @Override
-    //미구현
+    /**유저가 가지는 계좌 조회*/
     public List<Account> findAllByUserId(String userId) {
-        return List.of();
+        return accounts.stream().filter(account -> account.getUserId().equals(userId)).collect(Collectors.toList());
     }
     //미구현
     @Override
