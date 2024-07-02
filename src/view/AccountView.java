129963469411a64e9ef70aac02d0e84997475cda
jy <ju79318@naver.com>
2024-07-02 17:27:16 +0900

    feat:계좌 조회 출력 코드

--- src/view/AccountView.java
+++ src/view/AccountView.java
@@ -19,8 +19,13 @@ public class AccountView {
     public String printCheckPassword(){
         return PrintUtil.readLine("비밀번호 재확인 : ");
     }
+    public void printAccountCreateSuccess(){PrintUtil.println("계좌 생성이 완료 되었습니다. ");}
     public void wrongPassword() {
-        PrintUtil.print("설정한 비밀번호와 일치하지 않습니다.");
+        PrintUtil.println("설정한 비밀번호와 일치하지 않습니다.");
     }
+
+    public void startBalancePrint(String userId) {PrintUtil.println(userId+"의 잔액조회입니다.");}
+    public void printBalance(String accountNumber,Double balance) { PrintUtil.println(accountNumber+"의 잔액은 "+balance); }
+    public void printTotalBalance(Double totalBalance) {PrintUtil.println("  총 액은 "+totalBalance+"입니다.");}
 
 }
