4b553d075f676cf22b34592518b8d855d95f54d9
Dowon <tnstjs1919@gmail.com>
2024-07-02 17:27:25 +0900

    docs: 수정된 string list<Transaction>으로 원상복구

--- src/service/TransactionServiceImpl.java
+++ src/service/TransactionServiceImpl.java
@@ -2,10 +2,12 @@ package service;
 
 import domain.Account;
 import domain.Transaction;
+import view.ViewTransactionService;
+
+import java.util.List;
 
 public class TransactionServiceImpl implements TransactionService {
 
-    Transaction transaction = new Transaction();
     Account account = new Account();
     public TransactionServiceImpl() {
         super();
@@ -14,7 +16,7 @@ public class TransactionServiceImpl implements TransactionService {
     @Override
     public boolean deposit(String accountNumber, double amount) {
         if(account.getAccountNumber().equals(accountNumber)){
-            account.setBalance(amount);
+            account.setBalance(account.getBalance() + amount);
         }
         return false;
     }
@@ -23,21 +25,24 @@ public class TransactionServiceImpl implements TransactionService {
     public boolean withdraw(String accountNumber, String password, double amount) {
         if(account.getAccountNumber().equals(accountNumber) && account.getPassword().equals(password)){
             if(account.getBalance() >= amount) {
-                account.setBalance(amount);
+                account.setBalance(account.getBalance() - amount);
             }
         }
         return false;
     }
 
-    @Override
-    public String getTransactionsByAccountNumber(String accountNumber) {
-        if(account.getAccountNumber() == accountNumber){
-            return accountNumber.toString();
-        }else {
-            return "존재하지 않는 계좌번호";
-        }
 
+
+    @Override
+    public List<Transaction> getTransactionsByAccountNumber(String accountNumber) {
+
+        return null;
     }
 
+    // 입금 입력받으면 계좌번호, 입금금액 날짜 총금액 출력 메서드
 
+
+    // 출금 입력받으면 계좌번호, 출금금액 날짜 총금액 출력 메서드
+
+    //상대계좌? - 입금메서드
 }
