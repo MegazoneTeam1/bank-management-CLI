0ec4e578a112a7f20e6eebddfaa78bcb377bf086
jy <ju79318@naver.com>
2024-07-02 19:46:44 +0900

    feat :  이름 업데이트 추가

--- src/repository/AccountRepositoryImpl.java
+++ src/repository/AccountRepositoryImpl.java
@@ -9,26 +9,29 @@ import java.util.stream.Collectors;
 public class AccountRepositoryImpl implements AccountRepository {
     private static List<Account> accounts = new ArrayList<>();
 
-    @Override
     /**생성된 계좌 저장*/
+    @Override
     public void save(Account account) {
         accounts.add(account);
     }
 
-    @Override
     /**계좌번호 검색*/
+    @Override
     public Account findByAccountNumber(String accountNumber) {
         return accounts.stream().filter(account -> account.getAccountNumber().equals(accountNumber)).findFirst().orElse(null);
     }
 
-    @Override
     /**유저가 가지는 계좌 조회*/
+    @Override
     public List<Account> findAllByUserId(String userId) {
         return accounts.stream().filter(account -> account.getUserId().equals(userId)).collect(Collectors.toList());
     }
-    //미구현
+    /**계좌 이름 변경*/
     @Override
-    public void update(Account account) {
-
+    public void update(String accountNumber,String changeName) {
+        Account account = findByAccountNumber(accountNumber);
+        account.setName(changeName);
     }
+
+
 }
