8b5e09da2fe411618e1809ddd93124634b108c9e
jy <ju79318@naver.com>
2024-07-02 17:26:12 +0900

    refactor: 매개변수 타입 변경

--- src/service/AccountService.java
+++ src/service/AccountService.java
@@ -1,11 +1,13 @@
 package service;
 
 import domain.Account.Account;
+import domain.Account.AccountBuilder;
+
 import java.util.List;
 
 public interface AccountService {
 
-    boolean createAccount(Account account);
+    boolean createAccount(AccountBuilder account);
 
     List<
         Account> getAccountsByUserId(String userId);
