aa04771c75043b4c7c7e8a067e96b3c7e7928a4b
Dowon <tnstjs1919@gmail.com>
2024-07-02 19:23:59 +0900

    feat: 거래저장소 저장 및 찾기

---
+++ src/repository/TransactionRepositoryImpl.java
@@ -0,0 +1,19 @@
+package repository;
+
+import domain.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionRepositoryImpl implements  TransactionRepository{
+    private static List<Transaction> transactions = new ArrayList<>();
+    @Override
+    public void save(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    @Override
+    public List<Transaction> findTransactionsByAccountNumber(String accountNumber) {
+        return transactions.stream().filter(transaction -> transaction.getAccountNumber().equals(accountNumber)).toList();
+    }
+}
