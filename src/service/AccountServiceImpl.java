4133bca38cf4ff81fa0e14d36dbd2e8bd587a676
jy <ju79318@naver.com>
2024-07-02 17:29:41 +0900

    refactor:메서드 명명 수정, 생성자 타입 변경

--- src/service/AccountServiceImpl.java
+++ src/service/AccountServiceImpl.java
@@ -1,11 +1,12 @@
 package service;
 
 import domain.Account.Account;
+import domain.Account.AccountBuilder;
 import domain.User;
-import repository.AccountRepository;
 import repository.AccountRepositoryImpl;
 import view.AccountView;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class AccountServiceImpl implements AccountService {
@@ -20,24 +21,22 @@ public class AccountServiceImpl implements AccountService {
     }
 
     @Override
-    public boolean createAccount(Account account) {
-        User user = new User();
-
+    public boolean createAccount(AccountBuilder account) {
         accountView.startPrint();
         //계좌 10자리 랜덤 생성
-        setAccountNum(account);
+        setAccountNumber(account);
 
         //비밀번호 사용자에게서 받기
         String password = getPasswordToUser();
 
         //비밀번호 삽입
-        account.setPassword(password);
+        account.password(password);
 
         //비밀번호 확인
         checkPasswordAgain(password);
 
         //계좌생성, 계좌저장소에 저장소
-        accountRepository.save(account);
+        accountRepository.save(account.build());
         return false;
     }
 
@@ -52,12 +51,14 @@ public class AccountServiceImpl implements AccountService {
     public double getTotalBalance(String userId) {
         return 0;
     }
-    /**@계좌번호 중복 확인*/
+
+
+    /**계좌번호 중복 확인*/
     public boolean checkAccountNum(String accountNumber){
         return accountRepository.findByAccountNumber(accountNumber)!= null;
     }
 
-    /**@비밀번호 4자리 체크*/
+    /**비밀번호 4자리 체크*/
     public String checkPasswordLength(String password){
         while(password.length() != PWLENGTH){
             //다시 입력
@@ -65,12 +66,12 @@ public class AccountServiceImpl implements AccountService {
         }
         return password;
     }
-    /**@비밀번호를 사용자로부터 받기*/
+    /**비밀번호를 사용자로부터 받기*/
     public String getPasswordToUser(){
         String password = accountView.printPassword();
         return checkPasswordLength(password);
     }
-    /**@비밀번호 재확인*/
+    /**비밀번호 재확인*/
     public boolean checkPasswordAgain(String password) {
         String checkPassword = accountView.printCheckPassword();
         while(!checkPassword.equals(password)){
@@ -79,21 +80,23 @@ public class AccountServiceImpl implements AccountService {
         }
         return true;
     }
-   /**@계좌 생성에 삽입*/
-    public void setAccountNum(Account account){
+   /**계좌 생성에 삽입*/
+    public void setAccountNumber(AccountBuilder account){
         //계좌 번호 생성
         String accountNumber = generateUniqueAccountNumber();
         accountView.printAccount(Integer.parseInt(accountNumber));
         //계좌 번호 삽입
-        account.setAccountNumber(accountNumber);
+        account.accountNumber(accountNumber);
     }
-    /**@계좌번호 랜덤으로 10자리 받기*/
+    /**계좌번호 랜덤으로 10자리 받기*/
     public String generateUniqueAccountNumber(){
         int accountNumber;
         do {
             accountNumber = (int) (Math.random() * ACCOUNTLENGTH)+ACCOUNTLENGTH;
         }while (checkAccountNum(String.valueOf(accountNumber)));
         return String.valueOf(accountNumber);
+
+
     }
 
 }
