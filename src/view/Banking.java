1dbe6c5572516412062bbe19f92ed70c03190a9f
jeongmin <jeongmin0046@gmail.com>
2024-07-03 20:37:51 +0900

    refactor: view에서 service로 분리

--- src/view/Banking.java
+++ src/view/Banking.java
@@ -1,32 +1,14 @@
 package view;
 
 public class Banking {
-    public void bankingservice() {
+    public int bankingService() {
 
-        String[] options = {"계좌 생성", "계좌 조회", "잔액 조회", "입금", "출금","로그아웃"};
+        String[] options = {"계좌 생성", "계좌 조회", "잔액 조회", "입금", "출금", "로그아웃"};
+        PrintUtil.println("");
+        PrintUtil.println("계좌 관리 시스템");
+        PrintUtil.displayMenu(options);
+        return PrintUtil.readInt("뭐할거임?");
 
-        while (true) {
-            PrintUtil.displayMenu(options);
-            int choice = PrintUtil.readInt("뭐할거임?");
-            switch(choice) {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    return;
-                default:
-                    PrintUtil.println("잘못된 입력입니다.");
-                    PrintUtil.println("");
-            }
-
-        }
 
     }
 
