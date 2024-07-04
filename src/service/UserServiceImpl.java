1b14b67eaaf18b3714e67fcca96b0079798d850d
jeongmin <jeongmin0046@gmail.com>
2024-07-04 18:11:06 +0900

    refactor: password 길이 제한 상수로 표현

--- src/service/UserServiceImpl.java
+++ src/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package service;
 
+import domain.Account.AccountBuilder;
 import domain.User;
 import repository.UserRepository;
 import repository.UserRepositoryImpl;
@@ -9,15 +10,17 @@ import view.UserView;
 
 public class UserServiceImpl implements UserService {
 
+    final static private int LENGTH_PASSWORD = 6;
+
     UserRepository userRepository = new UserRepositoryImpl();
     Banking banking = new Banking();
     UserView userView = new UserView();
     MainMenu mainMenu = new MainMenu();
-
+    AccountService accountService = new AccountServiceImpl();
 
     /**
      *
-     * @param user 를 List에 저장
+     * @param user user를 List에 저장
      * @return true를 반환하여 사용자 등록
      */
     @Override
@@ -29,9 +32,9 @@ public class UserServiceImpl implements UserService {
 
     /**
      *
-     * @param id
-     * @param password
-     * @return
+     * @param id List에 user id가 있고
+     * @param password List에 user가 null이 아니고 비밀번호가 일치하면
+     * @return user를 반환, 없을 시 null을 반환
      */
     @Override
     public User login(String id, String password) {
@@ -42,16 +45,17 @@ public class UserServiceImpl implements UserService {
         return null;
     }
 
-    /**
-     *
-     * @param user
-     * @return
-     */
+    // 미구현
     @Override
     public boolean updateUser(User user) {
         return true;
     }
 
+    /**
+     *
+     * @param id List에 있는 id와 비교
+     * @return null이 아니면 true, null이면 false를 반환
+     */
     public boolean duplicateID(String id) {
         User user = userRepository.findById(id);
         if (user != null) {
@@ -60,8 +64,9 @@ public class UserServiceImpl implements UserService {
         return false;
     }
 
+
     public void signUp() {
-        String name = null;
+        String name;
         while (true) {
             name = userView.inputName();
             if(name.isEmpty()) {
@@ -71,7 +76,7 @@ public class UserServiceImpl implements UserService {
             }
         }
 
-        String phoneNumber = null;
+        String phoneNumber;
         while (true) {
             phoneNumber = userView.inputPhoneNumber();
             if(phoneNumber.isEmpty()) {
@@ -82,7 +87,7 @@ public class UserServiceImpl implements UserService {
         }
 
 
-        String id = null;
+        String id;
         while (true) {
             id = userView.inputId();
             if(id.isEmpty()) {
@@ -94,11 +99,10 @@ public class UserServiceImpl implements UserService {
             }
         }
 
-        /** 무한반복문을 통해 비밀번호가 6자리가 아닐경우 다시 설정하고, 6자리가 맞으면 break 를 통해 회원가입이 성공.*/
-        String password = null;
+        String password;
         while (true) {
             password = userView.inputPassword();
-            if(password.length() == 6) {
+            if(password.length() == LENGTH_PASSWORD) {
                 break;
             } else {
                 userView.reEnterPassword();
@@ -107,7 +111,6 @@ public class UserServiceImpl implements UserService {
 
         User newuser = new User(name, phoneNumber, id, password);
 
-        /** 새로운 사용자 데이터를 입력받고 리스트에 저장하여 사용자 등록 */
         if(registerUser(newuser)) {
             userView.successSignup();
         } else  {
@@ -128,7 +131,7 @@ public class UserServiceImpl implements UserService {
             return;
         } else
             userView.successLogin();
-        accountPage();
+        accountPage(id);
     }
 
 
@@ -144,6 +147,8 @@ public class UserServiceImpl implements UserService {
                     loginMenu();
                     break;
                 case 3:
+                    break;
+                case 4:
                     return;
                 default:
                     userView.wrongInput();
@@ -153,21 +158,26 @@ public class UserServiceImpl implements UserService {
         }
     }
 
-    public void accountPage() {
+    public void accountPage(String userid) {
         while (true) {
             int choice = banking.bankingService();
             switch (choice) {
                 case 1:
+                    AccountBuilder account = new AccountBuilder().userId(userid);
+                    accountService.createAccount(account);
                     break;
                 case 2:
                     break;
                 case 3:
+                    accountService.getTotalBalance(userid);
                     break;
                 case 4:
                     break;
                 case 5:
                     break;
                 case 6:
+                    break;
+                case 7:
                     return;
                 default:
                     userView.wrongInput();
